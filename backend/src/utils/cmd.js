7a1a08e7662374839fec7bb4ff9b029287ca52eb
foamzou <foamzou@gmail.com>
2022-08-06 15:20:27 +0800

    fix(secure): command rejection

--- backend/src/utils/cmd.js
+++ backend/src/utils/cmd.js
@@ -1,25 +1,32 @@
-const logger = require('consola');
-const execAsync = require('child_process').exec;
+const logger = require("consola");
+const spawnAsync = require("child_process").spawn;
 
-module.exports = function exec(cmd) {
-    return new Promise((resolve, reject) => {
-        execAsync(cmd, (error, stdout, stderr) => {
-            if (error) {
-                logger.error(`exec error: ${error}`);
-                resolve({
-                    code: error.code,
-                    message: error.message
-                });
-                return;
-            }
-            if (stderr) {
-                logger.error(`exec stderr: ${stderr}`);
-                return;
-            }
-            resolve({
-                code: 0,
-                message: stdout
-            });
-        });
+module.exports = function exec(exe, args) {
+  return new Promise(function (resolve, reject) {
+    console.log(exe, args);
+    const process = spawnAsync(exe, args);
+    let stdout = "";
+    let stderr = "";
+
+    process.stdout.on("data", function (data) {
+        stdout += data;
     });
+    process.stderr.on("data", function (data) {
+      stderr += data;
+    });
+
+    process.on("close", function (code) {
+      resolve({
+        code: stderr ? code : 0,
+        message: stderr ? stderr: stdout,
+      });
+    });
+    process.on("error", function (error) {
+      logger.error('exec error: ', error);
+      resolve({
+        code: -1,
+        message: error.message,
+      });
+    });
+  });
 }
